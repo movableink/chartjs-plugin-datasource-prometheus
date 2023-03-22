fb3aa080e8f81bb0dcb649599a03d4a73583abbf
Michael Nutt <michael@nuttnet.net>
2023-03-22 16:45:19 -0400

    ignore dist/

--- dist/axes.d.ts
+++
@@ -1,4 +0,0 @@
-import { Chart } from "chart.js/auto";
-import { ChartDatasourcePrometheusPluginOptions } from "./options";
-export declare function setTimeAxesOptions(chart: Chart): void;
-export declare function fillGaps(chart: Chart, start: Date, end: Date, step: number, options: ChartDatasourcePrometheusPluginOptions): void;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ChartDatasourcePrometheusPlugin } from './plugin';
-declare const _default: ChartDatasourcePrometheusPlugin;
-export default _default;
